94ccdfb13f03f31875bc00c1c324ae181ef84eb8
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-13 22:49:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import anderson_ksamp
 
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.05
-    statistic,pvalue = ks_2samp(x, y)
+    pvalue = anderson_ksamp([x, y])[2]
     return pvalue < alpha
